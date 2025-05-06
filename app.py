f35491a0ffaa4b5a2903a15236735dac1673a4b6
Ashutosh kumar <ashu@Ashutoshs-MacBook-Air.local>
2025-05-06 07:14:22 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html'
+                           )
+@app.route('/submit', methods=['POST'])
+def submit():
+    selected_task = request.form.getlist('tasks')
+    return f"You completed: {','
+    .join(selected_task)}"
+if __name__ == '__main__':
+    app.run(debug=True)
